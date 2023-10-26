cffbcc515f7171488d9b7f899ad3114daedb40cf
Satriowish <143361632+Satriowish@users.noreply.github.com>
2023-10-26 15:10:07 +0700

    Add files via upload

---
+++ Jobsheet9/LinearSearch26.java
@@ -0,0 +1,16 @@
+public class LinearSearch26 {
+    public static void main(String[] args) {
+
+        int[] arrayInt = { 34, 18, 26, 48, 72, 20, 56, 63 };
+        int key = 20;
+        int hasil = 0;
+        for (int i = 0; i < arrayInt.length; i++) {
+            if (arrayInt[i] == key) {
+                hasil = i;
+                break;
+            }
+        }
+
+        System.out.println("Key ada dalam array pada posisi indeks ke-" + hasil);
+    }
+}
