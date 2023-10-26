8e67c90f6859b0da85f35f116c848bd2ce301af6
Satriowish <143361632+Satriowish@users.noreply.github.com>
2023-10-26 15:20:29 +0700

    Add files via upload

--- Jobsheet9/LinearSearch26.java
+++ Jobsheet9/LinearSearch26.java
@@ -1,16 +1,27 @@
+import java.util.Scanner;
+
 public class LinearSearch26 {
     public static void main(String[] args) {
-
-        int[] arrayInt = { 34, 18, 26, 48, 72, 20, 56, 63 };
-        int key = 20;
-        int hasil = 0;
+        Scanner input = new Scanner(System.in);
+        int key = 0;
+        int hasil = 0, jml, status = 1;
+        System.out.print("Masukkan jumlah elemen array : ");
+        jml = input.nextInt();
+        int[] arrayInt = new int[jml];
         for (int i = 0; i < arrayInt.length; i++) {
-            if (arrayInt[i] == key) {
-                hasil = i;
-                break;
+            System.out.print("Masukkan elemen array ke-" + i + " : ");
+            arrayInt[i] = input.nextInt();
+        }
+        System.out.print("Masukkan key yang ingin dicari : ");
+        key = input.nextInt();
+        for (int i = 0; i < arrayInt.length; i++) {
+            if (arrayInt[i] == key && key == 0) {
+                System.out.println("Masukkan key yang ingin dicari : 0");
+                status = -1;
+                System.exit(status);
             }
+            break;
         }
 
-        System.out.println("Key ada dalam array pada posisi indeks ke-" + hasil);
     }
 }
