b070b5c4261f87c7a125ac50f26492f0cd13eb2b
Satriowish <143361632+Satriowish@users.noreply.github.com>
2023-10-26 11:52:41 +0700

    Add files via upload

---
+++ Jobsheet1/latihan26.java
@@ -0,0 +1,13 @@
+/**
+ * latihan26
+ */
+public class latihan26 {
+
+    public static void main(String[] args) {
+        System.out.println("Nama    :Satrio Wisnu Adi Pratama");
+        System.out.println("Absen   :26");
+        System.out.println("Kelas   :D4TI 1F");
+        System.out.println("Jurusan :Teknologi Informasi");
+
+    }
+}
