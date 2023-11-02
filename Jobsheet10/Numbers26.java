375de885730732a705b8f031619aead6c1e1d579
Satriowish <143361632+Satriowish@users.noreply.github.com>
2023-11-02 16:01:01 +0700

    percobaan 3

---
+++ Jobsheet10/Numbers26.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class Numbers26 {
+    public static void main(String[] args) {
+        int[][] myNumbers = new int[3][5];
+        myNumbers[0] = new int[5];
+        myNumbers[1] = new int[3];
+        myNumbers[2] = new int[1];
+
+        for (int i = 0; i < myNumbers.length; i++) {
+            System.out.println(Arrays.toString(myNumbers[i]));
+        }
+        for (int i = 0; i < myNumbers.length; i++) {
+            System.out.println("Panjang Bari ke- " + (i + 1) + ":" + myNumbers[i].length);
+        }
+    }
+}
