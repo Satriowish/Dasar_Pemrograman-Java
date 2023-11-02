df5247413ad72aca838858a59c66c20f12b4e23f
Satriowish <143361632+Satriowish@users.noreply.github.com>
2023-11-02 14:22:39 +0700

    percobaan 1
    2.1 Percobaan 1: Deklarasi, Inisialisasi, dan Menampilkan Array 2 Dimensi

---
+++ Jobsheet10/Bioskop26.java
@@ -0,0 +1,26 @@
+public class Bioskop26 {
+    public static void main(String[] args) {
+        String[][] penonton = new String[4][2];
+        penonton[0][0] = "Amin";
+        penonton[0][1] = "Bena";
+        penonton[1][0] = "Candra";
+        penonton[1][1] = "Dela";
+        penonton[2][0] = "Eka";
+        penonton[2][1] = "Farhan";
+        penonton[3][0] = "Gisel";
+        penonton[3][1] = "Hana";
+        System.out.printf("%s \t %s\n", penonton[0][0], penonton[0][1]);
+        System.out.printf("%s \t %s\n", penonton[1][0], penonton[1][1]);
+        System.out.printf("%s \t %s\n", penonton[2][0], penonton[2][1]);
+        System.out.printf("%s \t %s\n", penonton[3][0], penonton[3][1]);
+        System.out.println(penonton.length);
+        for (String[] barispenonton : penonton) {
+            System.out.println("Panjang baris : " + barispenonton.length);
+        }
+        System.out.println("Semua Penonton pada setiap Baris");
+        for (int i = 0; i < penonton.length; i++) {
+            System.out.println("Penonton pada baris ke-" + (i + 1) + ":" + String.join(",", penonton[i]));
+        }
+
+    }
+}
