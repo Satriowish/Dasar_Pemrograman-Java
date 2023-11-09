9615e4f8cf58f68d264cdbb2ddd854bd30a54ae7
Satriowish <143361632+Satriowish@users.noreply.github.com>
2023-11-09 14:24:42 +0700

    perbaikan segitiga

--- Jobsheet11/Triangle.java
+++ Jobsheet11/Triangle.java
@@ -2,17 +2,18 @@ import java.util.Scanner;
 
 public class Triangle {
     public static void main(String[] args) {
-        Scanner input26 = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         System.out.print("Masukkan nilai N = ");
-        int N = input26.nextInt();
+        int N = input.nextInt();
 
-        int i = 0;
+        int i = 1;
         while (i <= N) {
-            int j = 0;
-            while (j < i) {
+            int j = 1;
+            while (j <= i) {
                 System.out.print("*");
                 j++;
             }
+            System.out.println();
             i++;
         }
     }
