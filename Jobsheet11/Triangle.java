1b7fbfb2f8819066043559e6fcba0ac3581f4a1f
Satriowish <143361632+Satriowish@users.noreply.github.com>
2023-11-09 14:15:29 +0700

    percobaan 3

---
+++ Jobsheet11/Triangle.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Triangle {
+    public static void main(String[] args) {
+        Scanner input26 = new Scanner(System.in);
+        System.out.print("Masukkan nilai N = ");
+        int N = input26.nextInt();
+
+        int i = 0;
+        while (i <= N) {
+            int j = 0;
+            while (j < i) {
+                System.out.print("*");
+                j++;
+            }
+            i++;
+        }
+    }
+}
