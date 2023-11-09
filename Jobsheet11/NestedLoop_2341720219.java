693cc84cd75696bfa532da477498f106eee8c2c6
Satriowish <143361632+Satriowish@users.noreply.github.com>
2023-11-09 15:15:57 +0700

    Modifikasi Foreach

--- Jobsheet11/NestedLoop_2341720219.java
+++ Jobsheet11/NestedLoop_2341720219.java
@@ -16,8 +16,8 @@ public class NestedLoop_2341720219 {
 
         for (int i = 0; i < temps.length; i++) {
             System.out.print("Kota ke-" + (i + 1) + ": ");
-            for (int j = 0; j < temps[i].length; j++) {
-                System.out.print(temps[i][j] + " ");
+            for (double temp : temps[i]) {
+                System.out.print(temp + " ");
             }
             System.out.println();
         }
