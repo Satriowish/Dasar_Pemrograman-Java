083c0a401dabc288a6840b2ddc97371e4e7f9ce9
Satriowish <143361632+Satriowish@users.noreply.github.com>
2023-11-09 14:49:26 +0700

    Quiz modif

--- Jobsheet11/Quiz.java
+++ Jobsheet11/Quiz.java
@@ -14,11 +14,18 @@ public class Quiz {
                 System.out.print("Tebak angka (1-10): ");
                 int answer = input.nextInt();
                 input.nextLine();
-                success = (answer == number);
+
+                if (answer < number) {
+                    System.out.println("Angka yang Anda tebak terlalu kecil.");
+                } else if (answer > number) {
+                    System.out.println("Angka yang Anda tebak terlalu besar.");
+                } else {
+                    success = true;
+                }
             } while (!success);
-            System.out.print("Apakah Anda ingin mengulang permainan (Y/y)?");
+
+            System.out.print("Apakah Anda ingin mengulang permainan (Y/y)? ");
             menu = input.nextLine().charAt(0);
         } while (menu == 'y' || menu == 'Y');
-
     }
 }
