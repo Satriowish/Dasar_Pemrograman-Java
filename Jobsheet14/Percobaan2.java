b42847be44cf8950ea9a5782e5a0cfe9211eb1bb
Satriowish <143361632+Satriowish@users.noreply.github.com>
2023-11-30 14:03:32 +0700

    Modifikasi Pecobaan 2

--- Jobsheet14/Percobaan2.java
+++ Jobsheet14/Percobaan2.java
@@ -1,24 +1,35 @@
 import java.util.Scanner;
 
 public class Percobaan2 {
-
     static int hitungPangkat(int x, int y) {
         if (y == 0) {
-            return (1);
+            return 1;
         } else {
             return (x * hitungPangkat(x, y - 1));
         }
     }
 
+    static void cetakDeretPangkat(int x, int y) {
+        if (y == 0) {
+            System.out.println("1");
+        } else {
+            System.out.print(x + "x");
+            cetakDeretPangkat(x, y - 1);
+        }
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int Bilangan, Pangkat;
-        System.out.print("Bilangan yang dihitung : ");
-        Bilangan = sc.nextInt();
-        System.out.print("Pangkat : ");
-        Pangkat = sc.nextInt();
-        System.out.println(hitungPangkat(Bilangan, Pangkat));
+        int bilangan, pangkat;
 
+        System.out.print("Bilangan yang dihitung : ");
+        bilangan = sc.nextInt();
+        System.out.print("Pangkat : ");
+        pangkat = sc.nextInt();
+
+        System.out.println("Hasil perpangkatan : " + hitungPangkat(bilangan, pangkat));
+
+        cetakDeretPangkat(bilangan, pangkat);
     }
 
 }
